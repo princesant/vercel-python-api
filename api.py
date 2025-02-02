6fe987b3c66a9107064b2f109c266890f01eb7a3
Prince Sant <princessant89@gmail.com>
2025-02-02 23:47:37 +0530

    Initial commit

--- api.py
+++ api.py
@@ -2,22 +2,21 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
-# Load the student data from the JSON file
-with open("q-vercel-python.json", "r") as file:
-    student_data = json.load(file)
-
 app = FastAPI()
 
-# Enable CORS (Allows requests from any website)
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
+# Load student marks
+with open("q-vercel-python.json", "r") as f:
+    data = json.load(f)
+
 @app.get("/api")
 async def get_marks(name: list[str] = Query([])):
-    marks = [student_data.get(n, None) for n in name]
+    marks = [data.get(n, None) for n in name]
     return {"marks": marks}
